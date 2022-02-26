a7149f80e7cf8fd0eb55874c6cce9f6d9e767936
Gopal Ramesh Dahale <dahalegopal27@gmail.com>
2022-02-26 21:42:36 +0530

    updated tests

--- ivav/tests/test_not_gate.py
+++ ivav/tests/test_not_gate.py
@@ -1,10 +1,12 @@
-from ivav.index_view import IndexView
+"""Tests for the not gate."""
 import pytest
+from ivav.index_view import IndexView
 
+STATE = [3, 1, 4, 1, 5, 9, 2, 6]
 PARAMETERS = [
-    [3, [3, 1, 4, 1, 5, 9, 2, 6], [1, 3, 1, 4, 9, 5, 6, 2], 0],
-    [3, [3, 1, 4, 1, 5, 9, 2, 6], [4, 1, 3, 1, 2, 6, 5, 9], 1],
-    [3, [3, 1, 4, 1, 5, 9, 2, 6], [5, 9, 2, 6, 3, 1, 4, 1], 2],
+    [3, STATE.copy(), [1, 3, 1, 4, 9, 5, 6, 2], 0],
+    [3, STATE.copy(), [4, 1, 3, 1, 2, 6, 5, 9], 1],
+    [3, STATE.copy(), [5, 9, 2, 6, 3, 1, 4, 1], 2],
 ]
 
 
@@ -13,4 +15,4 @@ PARAMETERS = [
 def test_not_gate(n, state_vector, expected_state_vector, index):
     index_view = IndexView(n, state_vector)
     index_view.x(index)
-    assert index_view.state_vector == expected_state_vector
+    assert all(index_view.state_vector == expected_state_vector)
