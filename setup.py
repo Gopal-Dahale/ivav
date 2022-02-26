1dd68f05b07c16832a0084986071cddf54642a00
Gopal Ramesh Dahale <dahalegopal27@gmail.com>
2022-02-26 09:53:52 +0530

    added long description in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
-setup(name="ivav",
-      version="0.0.1",
-      description="A quantum computer simulator",
-      py_modules=["index_view"],
-      package_dir={"": "ivav"}
-      classifiers=[
-            "Programming Language :: Python :: 3.9.7",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-      ]
-      )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ivav",
+    version="0.0.1",
+    description="A quantum computer simulator",
+    py_modules=["index_view"],
+    package_dir={"": "ivav"},
+    classifiers=[
+        "Programming Language :: Python :: 3.9.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
